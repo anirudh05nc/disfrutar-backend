bd4c4925a41264e6f7559e4ce04ba8dd95303264
Anirudh NC <154583974+anirudh05nc@users.noreply.github.com>
2026-09-01 22:00:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,6 @@ app = FastAPI(
 
 origins = [
     "https://outbreak.gfgkare.in",
-    "http://localhost",
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://localhost:4173",
-    "http://127.0.0.1",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:4173",
 ]
 
 # Enable CORS for frontend integration
