7a39642577638c39ff60f8691eba74affcbf2536
anirudh05nc <anirudh05nc@gmail.com>
2026-09-03 12:03:02 +0530

    URL Update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://disfrutar.gfgkare.in",
+    "https://disfrutar.anc-anirudh.online",
 ]
 
 # Enable CORS for frontend integration
